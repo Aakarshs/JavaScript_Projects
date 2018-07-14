f6cbfb7e329529dd8c20935342cd71e947f4192e
Aakarsh Sinha <aakarshs@outlook.com>
2018-07-14 20:21:01 +0530

    Update arabictoroman.js

--- arabictoroman.js
+++ arabictoroman.js
@@ -1,97 +1,79 @@
-//JavaScript-C24.2.0 (SpiderMonkey)
+function convertToRoman (num)
+{
 
-function checkCashRegister(price,cash,cid){
+    var roman={
+        1:"I",
+        4:"IV",
+        5:"V",
+        9:"IX",
+        10:"X",
+        40:"XL",
+        50:"L",
+        90:"XC",
+        100:"C",
+        400:"CD",
+        500:"D",
+        900:"CM",
+        1000:"M",
+    }
 
-    var values=[["PENNY", 0.01],
-                ["NICKEL", 0.05],
-                ["DIME", 0.1],
-                ["QUARTER", 0.25],
-                ["ONE", 1],
-                ["FIVE", 5],
-                ["TEN", 10],
-                ["TWENTY", 20],
-                ["ONE HUNDRED", 100]]
-    
-    var answer={
-        status:"",
-        change:[],
-    };
-    
-   
+    var mul={
+        1:1,
+        2:10,
+        3:100,
+        4:1000,
+    }
+
+    var romanarr=[1,4,5,9,10,40,50,90,100,400,500,900,1000];
 
     var arr=[];
-    var num_of_notes=[];
-    var x=cash-price;
-    var total=0;
-    var status;
-    
-    for(var i=0; i < values.length;i++){
-        num_of_notes.push(Math.round((cid[i][1]/values[i][1])));
-        arr.push(values[i][1]);
-        total=total+cid[i][1];
-    }    
-    
- 
-    arr=arr.reverse();
-    num_of_notes=num_of_notes.reverse();
-    var cid_rev=cid.reverse();
- 
-    var count=0;
-    var result=[];
-    
-    
-    
-    for(var i=0; i < arr.length;i++){
-        var notes=num_of_notes[i];
-           if(arr[i]<=x){
-              while(notes>0 && arr[i]<=x){
-                x=(x-arr[i]).toFixed(2);
-                count=count+arr[i]
-                
-                notes=notes-1;
-                total=total-arr[i]
-              //  total=total-arr[i];
-              }
-               
-            }
-            result.push([cid_rev[i][0],Math.round(count*100)/100]);
-               count=0;
-       }
-        
+    var finalarr=[];
+    var str=num.toString().split("");
+    var largest=0;
 
-
-    result = result.sort(function(a, b) {
-    return a[1] - b[1];});
-    answer.change=result.reverse();   
- 
-    
-    if(x> 0){
-        answer.status="INSUFFICIENT_FUNDS";
-        answer.change=[];
-    }
-    else if(total<=0 && x<= 0){
-        answer.status="CLOSED";
-      
-    }
-    else{
-        answer.status="OPEN";
-    }
-    
-    if (answer.status=="OPEN"){
-         var len=answer.change.length;
-        for(var i=0;i<len;i++){
-            if(answer.change[i][1]==0){
-                answer.change.splice(i,1);
-                i=0;
-                len=answer.change.length;
+    while(str.length>0){
+        for(var i =0;i<str.length;i++){
+            arr.push(str[0]*mul[str.length]);
+            str.shift();
             }
         }
-    }
-   
- 
+        i=0;
     
-    //print (answer[0]);
-    return(answer);
-};
+ 
+    for(var i=0;i<arr.length;i++){
+        if (arr[i] in roman){
+            finalarr.push(roman[arr[i]]);
+        }
+        else if (parseInt(arr[i])===0){
+            continue;
+        }
+        else if(parseInt(arr[i])>1000){
+            
+            largest=parseInt((arr[i].toString().split("")[0]));
+            for(var d =1;d<=largest;d++){
+            finalarr.push("M");
+            }
+        }
+        else{
+            var x = parseInt(arr[i]);
+            while(x!=0){
+                for(var j=0;j<romanarr.length;j++){
+                    if (x<romanarr[j]){
+                        finalarr.push(roman[romanarr[j-1]]);
+                        x=x-(romanarr[j-1])
+                        break;
+                    }
+                }
+            j=0;
+        }
+    }
+}
+    
 
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+
+return (finalarr.join(""));
+
+}
+
+
+convertToRoman(2014);
